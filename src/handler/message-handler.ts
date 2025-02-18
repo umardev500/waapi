1d4d4daf128f3a3cfdf9043da97488770bf40634
Umar <umar.dev500@gmail.com>
2025-02-18 11:19:37 +0700

    feat: add subscribe to the jid

--- src/handler/message-handler.ts
+++ src/handler/message-handler.ts
@@ -1,4 +1,5 @@
 import { MessageUpsertType, WAMessage } from 'baileys';
+import { getSocket } from '../config/socket';
 import { MessageService } from '../service/message-service';
 
 const service = new MessageService();
@@ -8,26 +9,28 @@ const service = new MessageService();
  * @param m Incoming message event.
  */
 export const handleMessage = async (m: { messages: WAMessage[]; type: MessageUpsertType; requestId?: string }) => {
-    const message = m.messages[0];
-    const { key, messageTimestamp, pushName, message: content } = message;
+    const { messages } = m;
 
-    if (!key?.remoteJid) {
-        console.warn('⚠️ Message received without a valid remote JID.');
-        return;
-    }
+    for (const msg of messages) {
+        const { key, messageTimestamp, pushName, message: content } = msg;
+        const { remoteJid: jid, fromMe, id } = key;
+        if (fromMe || !pushName || !id || !jid) {
+            // Logging
+            console.log('❌ fromme ', fromMe, ' pushName ', !pushName, ' id ', !id, ' jid ', !jid);
 
-    if (content?.conversation) {
-        await service.processTextMessage(
-            key.remoteJid,
-            key.id!,
-            pushName!,
-            messageTimestamp as number,
-            content.conversation,
-        );
-    } else if (content?.imageMessage) {
-        console.log('📷 Image message received, processing...');
-        await service.processImageMessage();
-    } else {
-        console.log('⚠️ Unsupported message type received.');
+            continue;
+        }
+
+        await getSocket()?.presenceSubscribe(jid);
+        console.log(`✅ Subscribed to presence updates for ${jid}`);
+
+        if (content?.conversation) {
+            await service.processTextMessage(jid, id, pushName, messageTimestamp as number, content.conversation);
+        } else if (content?.imageMessage) {
+            console.log('📷 Image message received, processing...');
+            await service.processImageMessage();
+        } else {
+            console.log('⚠️ Unsupported message type received.');
+        }
     }
 };
